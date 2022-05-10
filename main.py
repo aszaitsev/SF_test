fad7144d28c062ad5c8eed06f8213ebdd9c36c02
AS Zaytsev <101526974+aszaitsev@users.noreply.github.com>
2022-05-10 23:50:27 +0300

    Practic 15.4.1

--- main.py
+++ main.py
@@ -1,70 +1,75 @@
 
-# ПРАКТИКУМ 15.4.2
+# ПРАКТИКУМ 15.4.1
 
-import json
+def changeText(text):
+    """
+    Функция принимает строку с текстом.
+    Убирает все знаки препинания и возвращает
+    список, состоящий из слов текста.
+    """
 
-with open('json_example.json', encoding='utf8') as f:
-    templates = json.load(f)
+    for i in '!"\'#$%&()*+-,/:;<=>?@[\\]^_{|}~':
+        text = text.replace(i, '')
 
-def CheckInt(item):
-    return isinstance(item, int)
+    return text.split()
 
-def CheckStr(item):
-    return isinstance(item, str)
 
-def CheckBool(item):
-    return isinstance(item, bool)
+def mostCommon(text, lenght=0):
+    """
+    Функция принимает список и ограничение по длине.
+    Возвращает самый часто встречающийся элемент.
+    Если есть несколько элементов с одинаковой самой большой частотой, то вернёт их все.
+    """
 
-def CheckUrl(item):
-    if isinstance(item, str):
-        return item.startswith('http://') or item.startswith('https://')
-    else:
-        return False
+    most_common = []
+    qty_most_common = 0
 
-def CheckStrValue(item, val):
-    if isinstance(item, str):
-        return item in val
-    else:
-        return False
 
-def ErrorLog(item, value, string):
-    Error.append({item: f' {value}, {string}'})
+    for item in text:
+        if len(item) > lenght:
+            qty = text.count(item)
+            if qty > qty_most_common and qty > 2:
+                qty_most_common = qty
+                most_common = [item]
+            elif qty == qty_most_common:\
+                most_common.append(item)
 
-listOfItems = {'timestamp': 'int', 'item_price': 'int',
-               'referer': 'url', 'location': 'url',
-               'item_url': 'url', 'remoteHost': 'str',
-               'partyId': 'str', 'sessionId': 'str',
-               'pageViewId': 'str', 'item_id': 'string',
-               'basket_price': 'str', 'userAgentName': 'str',
-               'eventType': 'str', 'detectedDuplicate': 'bool',
-               'detectedCorruption': 'bool', 'firstInSession': 'bool'}
+    return list(set(most_common))
 
-Error = []
-for items in templates:
-    for item in items:
-        if item in listOfItems:
-            if listOfItems[item] == 'int':
-                if not CheckInt(items[item]):
-                    ErrorLog(item, items[item], f'ожидали тип {listOfItems[item]}')
-            elif listOfItems[item] == 'str':
-                if not CheckStr(items[item]):
-                    ErrorLog(item, items[item], f'ожидали тип {listOfItems[item]}')
-            elif listOfItems[item] == 'bool':
-                if not CheckBool(items[item]):
-                    ErrorLog(item, items[item], f'ожидали тип {listOfItems[item]}')
-            elif listOfItems[item] == 'url':
-                if not CheckUrl(items[item]):
-                    ErrorLog(item, items[item], f'ожидали тип {listOfItems[item]}')
-            elif listOfItems[item] == 'val':
-                if not CheckStrValue(items[item], ['itemBuyEvent', 'itemViewEvent']):
-                    ErrorLog(item, items[item], f'ожидали  itemBuyEvent или itemViewEvent')
+
+def mostLenght(text):
+    """
+    Функция принимает список.
+    Возвращает самый длинный элемент.
+    Если есть несколько элементов с одинаковой самой большой длиной, то вернёт их все.
+    """
+
+
+    most_lenght = []
+    qty_most_lenght = 0
+    alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    for item in text:
+        for char in item:
+            if char not in alphabet:
+                charEn = False
             else:
-                ErrorLog(item, items[item], 'неожиданное значение')
-        else:
-            ErrorLog(item, items[item], 'неизвестная переменная')
+                charEn = True
 
-if Error == []:
-    print('Pass')
-else:
-    print('Fail')
-    print(Error)
+        if charEn:
+            qty = len(item)
+            if qty > qty_most_lenght:
+                qty_most_lenght = qty
+                most_lenght = [item]
+            elif qty == qty_most_lenght:
+                most_lenght.append(item)
+
+    return list(set(most_lenght))
+
+nameFile = input('Название файла: ')
+
+with open(nameFile, encoding='utf8') as f:
+    fileText = f.read()
+
+fileText = changeText(fileText)
+print(f'Список самых частых слов длинной более трёх символов: {mostCommon(fileText, 3)}')
+print(f'Список самых длинных английских слов: {mostLenght(fileText)}')
